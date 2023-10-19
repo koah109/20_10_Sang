d110cad747741549256bc15d370f013f54b3f02a
Koah109 <neverfall109@gmail.com>
2023-10-19 17:04:35 +0700

    20-10

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng choa Sangggg';
+const contentCard = 'Chúc Sang - người con gái anh yêu 20-10 vui vẻ và hạnh phúc. Vì chúng ta yêu xa nên anh không thể trao cho em một ngày lễ 20-10 trọn vẹn. Vậy nên anh làm cái này để bù đắp choa em. Yêu em siêu nhiều Sang à';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
